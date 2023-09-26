1408fe76b40217fb38ee8439bdba2c5b73158362
HP <HP@DESKTOP-QI2NRA2>
2023-09-26 16:39:37 +0700

    fix recommendation system

--- app/core/views.py
+++ app/core/views.py
@@ -101,9 +101,9 @@ def post_view(request, pk):
     # Get the post main tag
     main_tag = post.tag.first()
     # Recommend posts with same tag
-    recommend_posts_tag = Post.objects.filter(tag__name=main_tag.name).exclude(pk=post.pk).order_by('-created_at')[:5]
+    recommend_posts_tag = Post.objects.filter(tag__name=main_tag.name).exclude(pk=pk).order_by('-created_at')[:5]
     # Recommend posts
-    recommend_posts = Post.objects.all().order_by('-created_at')[:5]
+    recommend_posts = Post.objects.all().exclude(pk=pk).order_by('-created_at')[:5]
     # Increase the time post has been seen
     post.increment_click_count()
     return render(request, 'post_view.html', {'post': post, 'main_tag': main_tag, 'recommend_posts_tag': recommend_posts_tag, 'recommend_posts': recommend_posts})
